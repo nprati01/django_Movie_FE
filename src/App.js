e553ddb8c8c9c26bcb518a69409802cead1f3a75
nprati01 <nicole.e.prati@gmail.com>
2023-07-04 15:58:01 -0400

    movie list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import MovieList from './components/movie-list';
+
 
 function App() {
+  const [movies, setMovie] = useState([]);
+
+  useEffect(() =>{
+    fetch("http://127.0.0.1:8000/movies", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 6ece67e59df273c8837cb6c2e508ba921f96e167'
+      }
+    }).then(resp => resp.json())
+      .then(resp =>setMovie(resp))
+      .catch(error => console.log(error))
+
+  }, [])
+
+  const movieClicked = movie => {
+    console.log(movie.title);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie Rater</h1>
       </header>
+      <div className='layout'>
+      <MovieList movies={movies} movieClicked={movieClicked}/>
+      <div>Movie Detail</div>
+
+      </div>
     </div>
   );
 }
